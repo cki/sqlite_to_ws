56a3aca6ee49a29c8afddc32afe0a991e0829019
Tim Kirch <kirch.tim@gmail.com>
2020-10-01 20:24:13 +0200

    Tests for how we want our future webserver to behave

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,57 @@
 const assert = require('assert');
 
+describe('Webserver', function() {
+    const main = require('../src/main');
+    const http = require('http');
+    const request = require('./http_req');
+    const sqlite3 = require('sqlite3').verbose()
+
+    async function getMinimalServer(cb) {
+	const db = new sqlite3.Database(':memory:');
+	await new Promise( (resolve) => {
+	    db.run('CREATE TABLE cooltable (field1 text, field2 text)', resolve);
+	});
+
+	await new Promise( (resolve) => {
+	    db.run(
+		'INSERT INTO cooltable (field1, field2) '+
+		    'VALUES ("hello", "world")', resolve);
+	});
+	const server = await main.run(db, 'cooltable', 8000);
+	cb(server);
+    }
+
+    it('should drop requests for not existsing fields in table', function(done) {
+	getMinimalServer(function(server) {
+	    let reqObject = {'f__': 'hello'};
+	    request(reqObject, 'localhost', '/', 8000, function(data) {
+		assert.ok(data.length > 0);
+		data = JSON.parse(data)
+		assert.ok(data.errorMessage !== null);
+		assert.equal(data.errorMessage, 'wrong req');
+
+		server.close();
+		done();
+	    });
+	});
+    });
+
+    it('should answer requests for existsing fields in table', function(done) {
+    	getMinimalServer(function(server) {
+    	    let reqObject = {'field1': 'hello'};
+    	    request(reqObject, 'localhost', '/', 8000, function(data) {
+    		assert.ok(data.length > 0);
+    		data = JSON.parse(data)
+    		assert.ok(data.errorMessage == null);
+		assert.equal(data.length, 1);
+		assert.deepEqual(data[0], {'field1': 'hello', 'field2': 'world'});
+    		server.close();
+    		done();
+    	    });
+    	});
+    });
+})
+
 describe('Database library', function() {
     const dbLib = require('../src/db');
     const sqlite3 = require('sqlite3').verbose()
@@ -33,3 +85,4 @@ describe('Database library', function() {
 
 
 });
+
